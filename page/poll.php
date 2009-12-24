e12cf5c9e25320b23595f22f317c58d893fabe5c
XPac <xpac27@gmail.com>
2009-12-24 14:42:33 +0100

    [POLL] Fix a bug when no user is connected on poll page

--- page/poll.php
+++ page/poll.php
@@ -20,9 +20,18 @@ if ($rs_question['total'] != 0)
 
     $user_idList = array();
     $user_voted_idList = array();
-    foreach($_SESSION['user'] as $id => $data)
+    if (count($_SESSION['user']) != 0)
     {
-        $user_idList[] = $id;
+        foreach($_SESSION['user'] as $id => $data)
+        {
+            $user_idList[] = $id;
+        }
+        $rs_result = $db->select
+        ('
+            SELECT `user_id`, `answer_id`
+            FROM `user_result`
+            WHERE `question_id`=' . $question['id'] . ' AND `user_id` IN (' . implode(',', $user_idList) . ')
+        ', 0, 8);
     }
 
     $rs_answer = $db->select
@@ -31,13 +40,6 @@ if ($rs_question['total'] != 0)
         FROM `answer` AS a
         JOIN `question_answer` AS j ON j.answer_id = a.id
         WHERE j.question_id = ' . $_GET['id'] . '
-    ', 0, 8);
-
-    $rs_result = $db->select
-    ('
-        SELECT `user_id`, `answer_id`
-        FROM `user_result`
-        WHERE `question_id`=' . $question['id'] . ' AND `user_id` IN (' . implode(',', $user_idList) . ')
     ', 0, 8);
 
     $tpl->assignVar(array
@@ -57,35 +59,40 @@ if ($rs_question['total'] != 0)
                 'id'    => $answer['id'],
             ));
 
-            foreach ($rs_result['data'] as $result)
+            if (count($_SESSION['user']) != 0)
             {
-                if ($result['answer_id'] == $answer['id'])
+                foreach ($rs_result['data'] as $result)
                 {
-                    $tpl->assignLoopVar('answer.user', array
-                    (
-                        'login' => $_SESSION['user'][$result['user_id']]['login'],
-                        'id'    => $result['user_id'],
-                    ));
-                    $user_voted_idList[$result['user_id']] = true;
+                    if ($result['answer_id'] == $answer['id'])
+                    {
+                        $tpl->assignLoopVar('answer.user', array
+                        (
+                            'login' => $_SESSION['user'][$result['user_id']]['login'],
+                            'id'    => $result['user_id'],
+                        ));
+                        $user_voted_idList[$result['user_id']] = true;
+                    }
                 }
             }
         }
     }
 
-    foreach ($rs_result['data'] as $result)
+    if (count($_SESSION['user']) != 0)
     {
-        $poll_parameters['user'][$result['user_id']] = $result['answer_id'];
-    }
-
-    foreach($user_idList as $id)
-    {
-        if (!array_key_exists($id, $user_voted_idList))
+        foreach ($rs_result['data'] as $result)
         {
-            $tpl->assignLoopVar('user', array
-            (
-                'login' => $_SESSION['user'][$id]['login'],
-                'id'    => $id,
-            ));
+            $poll_parameters['user'][$result['user_id']] = $result['answer_id'];
+        }
+        foreach($user_idList as $id)
+        {
+            if (!array_key_exists($id, $user_voted_idList))
+            {
+                $tpl->assignLoopVar('user', array
+                (
+                    'login' => $_SESSION['user'][$id]['login'],
+                    'id'    => $id,
+                ));
+            }
         }
     }
 
