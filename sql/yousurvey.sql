bea9341648eeab1271208932e5aa24e763e7d9bb
XPac <xpac27@gmail.com>
2010-01-28 01:05:43 +0100

    [SQL] New tables structure

--- sql/yousurvey.sql
+++ sql/yousurvey.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 24, 2010 at 11:37 PM
+-- Generation Time: Jan 28, 2010 at 01:05 AM
 -- Server version: 5.0.67
 -- PHP Version: 5.3.1
 
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `answer` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `label` varchar(32) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
 
 -- --------------------------------------------------------
 
@@ -42,6 +42,7 @@ CREATE TABLE IF NOT EXISTS `category` (
   `status` tinyint(1) NOT NULL default '0',
   `position` int(10) unsigned NOT NULL default '0',
   `label` varchar(32) collate utf8_unicode_ci NOT NULL,
+  `guid` varchar(32) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
@@ -60,6 +61,21 @@ CREATE TABLE IF NOT EXISTS `feeling` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `friend`
+--
+
+CREATE TABLE IF NOT EXISTS `friend` (
+  `user_id_1` int(10) unsigned NOT NULL,
+  `user_id_2` int(10) unsigned NOT NULL,
+  `valided` tinyint(1) NOT NULL default '0',
+  `date` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`user_id_1`,`user_id_2`),
+  KEY `user_id_2` (`user_id_2`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `question`
 --
 
@@ -71,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `question` (
   `label` varchar(255) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `category_id` (`category_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=33 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=36 ;
 
 -- --------------------------------------------------------
 
@@ -146,6 +162,13 @@ CREATE TABLE IF NOT EXISTS `user_result` (
 --
 
 --
+-- Constraints for table `friend`
+--
+ALTER TABLE `friend`
+  ADD CONSTRAINT `friend_ibfk_1` FOREIGN KEY (`user_id_1`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `friend_ibfk_2` FOREIGN KEY (`user_id_2`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `question`
 --
 ALTER TABLE `question`
@@ -155,9 +178,9 @@ ALTER TABLE `question`
 -- Constraints for table `question_answer_feeling`
 --
 ALTER TABLE `question_answer_feeling`
-  ADD CONSTRAINT `question_answer_feeling_ibfk_3` FOREIGN KEY (`feeling_id`) REFERENCES `feeling` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `question_answer_feeling_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `question_answer_feeling_ibfk_2` FOREIGN KEY (`answer_id`) REFERENCES `answer` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `question_answer_feeling_ibfk_2` FOREIGN KEY (`answer_id`) REFERENCES `answer` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `question_answer_feeling_ibfk_3` FOREIGN KEY (`feeling_id`) REFERENCES `feeling` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `user_prognostic`
