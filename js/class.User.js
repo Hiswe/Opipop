bd6da9769ff0be4d817e3da88a4798809cea3811
XPac <xpac27@gmail.com>
2010-06-19 19:39:56 +0200

    [JS] Fix indentation

--- js/class.User.js
+++ js/class.User.js
@@ -1,74 +1,74 @@
 var User =
 {
 
-	addToFriend : function(friendId)
-	{
-		var link   = $('#addToFriend_' + friendId);
-		var action = link.attr('class');
-		var params =
-		{
-			friendId : friendId,
-			action   : action
-		};
+    addToFriend : function(friendId)
+    {
+        var link   = $('#addToFriend_' + friendId);
+        var action = link.attr('class');
+        var params =
+        {
+            friendId : friendId,
+            action   : action
+        };
 
-		if (action == 'remove' && !confirm('Are you sure you want to remove this user from your friends ?'))
-		{
-			return;
-		}
+        if (action == 'remove' && !confirm('Are you sure you want to remove this user from your friends ?'))
+        {
+            return;
+        }
 
-		link.html('');
+        link.html('');
 
-		$.post(ROOT_PATH + 'remote/user/addToFriend', params, User.addToFriendCallback);
-	},
+        $.post(ROOT_PATH + 'remote/user/addToFriend', params, User.addToFriendCallback);
+    },
 
-	addToFriendCallback : function(data)
-	{
-		if (data == 'register')
-		{
-			window.location = ROOT_PATH + 'register';
-		}
-		else if (data == 'reload')
-		{
-			window.location = window.location;
-		}
-		else
-		{
-			data = $.parseJSON(data);
-		}
+    addToFriendCallback : function(data)
+    {
+        if (data == 'register')
+        {
+            window.location = ROOT_PATH + 'register';
+        }
+        else if (data == 'reload')
+        {
+            window.location = window.location;
+        }
+        else
+        {
+            data = $.parseJSON(data);
+        }
 
-		var link = $('#addToFriend_' + data.friendId);
-		link.attr('class', '');
+        var link = $('#addToFriend_' + data.friendId);
+        link.attr('class', '');
 
-		if (data.action == 'add')
-		{
-			link.addClass('cancel');
-			link.html('Cancle friend request');
-		}
-		else if (data.action == 'cancel' || data.action == 'remove')
-		{
-			link.addClass('add');
-			link.html('Add to friends');
-		}
-	},
+        if (data.action == 'add')
+        {
+            link.addClass('cancel');
+            link.html('Cancle friend request');
+        }
+        else if (data.action == 'cancel' || data.action == 'remove')
+        {
+            link.addClass('add');
+            link.html('Add to friends');
+        }
+    },
 
-	requestFriend : function(friendId, accept)
-	{
-		if (accept)
-		{
-			$('#request_' + friendId).html('<span>accepted</span>');
-		}
-		else
-		{
-			$('#request_' + friendId).html('<span>rejected</span>');
-		}
+    requestFriend : function(friendId, accept)
+    {
+        if (accept)
+        {
+            $('#request_' + friendId).html('<span>accepted</span>');
+        }
+        else
+        {
+            $('#request_' + friendId).html('<span>rejected</span>');
+        }
 
-		var params =
-		{
-			friendId : friendId,
-			action   : (accept) ? 'accept' : 'cancel'
-		};
-		$.post(ROOT_PATH + 'remote/user/addToFriend', params);
-	}
+        var params =
+        {
+            friendId : friendId,
+            action   : (accept) ? 'accept' : 'cancel'
+        };
+        $.post(ROOT_PATH + 'remote/user/addToFriend', params);
+    }
 
 };
 
--- js/class.User_edit.js
+++ js/class.User_edit.js
@@ -1,26 +1,26 @@
 var User_edit =
 {
 
-	init : function()
-	{
-	},
+    init : function()
+    {
+    },
 
-	submit : function()
-	{
+    submit : function()
+    {
         var gender = Form.getCleanInputValue($('#user_edit_gender'));
         var zip    = Form.getCleanInputValue($('#user_edit_zip'));
 
-		if (zip.lenght == 0 || gender.length == 0)
-		{
-			alert('You must fill all the form\'s field !');
-			return false;
-		}
-		else
-		{
-			Form.disable($('user_edit'));
-			return true;
-		}
-	}
+        if (zip.lenght == 0 || gender.length == 0)
+        {
+            alert('You must fill all the form\'s field !');
+            return false;
+        }
+        else
+        {
+            Form.disable($('user_edit'));
+            return true;
+        }
+    }
 
 };
 
