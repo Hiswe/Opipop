e8914ae11990599be15ac4845fd920af5021f875
XPac <xpac27@gmail.com>
2010-06-21 15:18:59 +0200

    [LIGHBOX] Better hide effect

--- js/main.js
+++ js/main.js
@@ -11,11 +11,13 @@ function onReady()
     // NYROMODAL
     $.fn.nyroModal.settings.debug = false;
     $.fn.nyroModal.settings.bgColor = '#ffffff';
-    //$.fn.nyroModal.settings.autoSizable = false;
-    //$.fn.nyroModal.settings.resizable = false;
     $.fn.nyroModal.settings.minHeight = 50;
     $.fn.nyroModal.settings.minWidth = 50;
     $.fn.nyroModal.settings.closeButton = '<a href="#" class="nyroModalClose"></a>';
+    $.fn.nyroModal.settings.hideContent = function(elts, settings, callback)
+    {
+        callback();
+    };
 }
 $(document).ready(onReady);
 
